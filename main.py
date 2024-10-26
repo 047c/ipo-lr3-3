324c06b32ca991dbdd0cdf143b39eed94821925a
047c <maks.smirnov.smolensk@gmail.com>
2024-10-26 21:31:56 +0300

    added comments

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ month = int(input("Введите месяц (month): "))  # Получаем м
 result = "определена как "  # Объявляем конструктор
 if 1 <= day <= 31 and month == 3 or 1 <= day <= 30 and month == 4 or 1 <= day <= 31 and month == 5:  # Проверяем
     result += "Весна"  # Изменяем конструктор
-elif 1 <= day <= 30 and month == 6 or 1 <= day <= 31 and month == 7 or 1 <= day <= 31 and month == 7:
-    result += "Лето"
-elif 1 <= day <= 30 and month == 9 or 1 <= day <= 31 and month == 10 or 1 <= day <= 30 and month == 11:
-    result += "Осень"
-elif 1 <= day <= 31 and month == 12 or 31 >= day >= 1 == month or 1 <= day <= 28 and month == 2:
-    result += "Зима"
-else:
+elif 1 <= day <= 30 and month == 6 or 1 <= day <= 31 and month == 7 or 1 <= day <= 31 and month == 7: # Проверяем
+    result += "Лето" # Изменяем конструктор
+elif 1 <= day <= 30 and month == 9 or 1 <= day <= 31 and month == 10 or 1 <= day <= 30 and month == 11: # Проверяем
+    result += "Осень" # Изменяем конструктор
+elif 1 <= day <= 31 and month == 12 or 31 >= day >= 1 == month or 1 <= day <= 28 and month == 2: # Проверяем
+    result += "Зима" # Изменяем конструктор
+else: # Если любая из проверок не завершилась успешно
     result = "не " + result + "какой-либо сезон года."  # Обрабатываем исключение, изменяем конструктор
 print(f"Введенная дата {result} ")  # Выводим собранную строку.
